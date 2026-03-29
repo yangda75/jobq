40db5c71baf23709fea4ab466cde4a1fa2b17bfc
yda <billyang363@gmail.com>
2026-03-29 15:12:37 +0800

    fix: data race on Executor::Impl::workers

--- src/Executor.cpp
+++ src/Executor.cpp
@@ -1,6 +1,7 @@
 #include "Executor.h"
 #include "JobQ.h"
 #include "Worker.h"
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -13,15 +14,18 @@ struct Executor::Impl {
     std::vector<std::thread> worker_threads{};
     std::vector<Source *> sources{};
     std::vector<Worker> workers{};
-
+    std::mutex m{}; // guards access to workers vector, not worker_threads
     void run() {
-        // start worker threads
-        for (int i = 0; i < WORKER_THREADS_PER_EXECUTOR; i++) {
-            workers.emplace_back(q);
-            worker_threads.emplace_back(std::thread{[this, i]() {
-                auto &w = workers[i];
-                w.runForever();
-            }});
+        {
+            std::lock_guard lk{m};
+            // start worker threads
+            for (int i = 0; i < WORKER_THREADS_PER_EXECUTOR; i++) {
+                workers.emplace_back(q);
+                worker_threads.emplace_back(std::thread{[this, i]() {
+                    auto &w = workers[i];
+                    w.runForever();
+                }});
+            }
         }
         for (auto &t : worker_threads) {
             t.join();
@@ -37,8 +41,11 @@ struct Executor::Impl {
 
     void shutdown() {
         q.close();
-        for (auto &w : workers) {
-            w.stop();
+        {
+            std::lock_guard lk{m};
+            for (auto &w : workers) {
+                w.stop();
+            }
         }
         // drain the q, discarding
         while (auto job = q.popOne()) {
