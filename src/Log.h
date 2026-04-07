73bbd58181d070d19ee03a4224bf8766465c41f7
yda <billyang363@gmail.com>
2026-04-07 22:15:37 +0800

    fix: improve cmake

--- src/Log.h
+++ src/Log.h
@@ -1,17 +1,18 @@
 #pragma once
+#include "spdlog/common.h"
 #include <spdlog/spdlog.h>
 #include <utility>
 
 // very thin wrapper around spdlog
 namespace jobq {
 
-
-template <typename... Args> void loginfo(Args &&...args) {
-    spdlog::info(std::forward<Args>(args)...);
+template <typename... Args>
+void loginfo(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+    spdlog::info(fmt, std::forward<Args>(args)...);
 }
-
-template <typename... Args> void logerror(Args &&...args) {
-    spdlog::error(std::forward<Args>(args)...);
+template <typename... Args>
+void logerror(spdlog::format_string_t<Args...> fmt, Args &&...args) {
+    spdlog::error(fmt, std::forward<Args>(args)...);
 }
 
 } // namespace jobq
